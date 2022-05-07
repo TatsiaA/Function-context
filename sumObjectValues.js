6a8e2c4f6013efd5fcfb851b8131ec930f2ca74e
TatsiaA <taniokk@mail.ru>
2022-05-07 13:40:38 +0300

    fix: change solution using context

--- sumObjectValues.js
+++ sumObjectValues.js
@@ -1,17 +1,28 @@
-function sumObjectValues(obj) {
-  const values = Object.values(obj)
-  .filter(el => typeof el === "number")
-  .reduce(function(sum, a) {
+function sumObjectValues() {
+
+  function add(sum, a) {
     sum += a;
     return(sum);
-    }, 0);
-  return values;
-}
+    }
 
+  let values = [];
+  for (let arg of arguments) {
+    values = values.concat(Object.values(arg)).filter(el => typeof el === "number");
+  }
+  return [].reduce.call(values, add);
+}
 
 let object = {
   a: 2,
   name: 'John',
   age: 23
 };
-sumObjectValues(object) // 25
+
+
+let object1 = {
+  a: 25,
+  name: 'Jane',
+  age: 25
+};
+
+console.log(sumObjectValues(object, object1));
